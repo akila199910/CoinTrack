c27807100649c34ffb5fd07690c9489525e91e0c
Akila-PHP <143219353+akila199910@users.noreply.github.com>
2025-08-31 20:15:14 +0530

    add  sidebar links

--- client/src/app/components/Sidebar.tsx
+++ client/src/app/components/Sidebar.tsx
@@ -1,46 +1,84 @@
 import Link from "next/link";
+import { useRef } from "react";
+import Image from "next/image";
+import coin from '../../../public/coin.svg'
+import dashboard from '../icons/dashboard.svg'
+import profileW from '../icons/profilew.svg'
+import settings from '../icons/settingsW.svg'
+import logout from '../icons/logoutW.svg'
+import categories from '../icons/category.svg'
 
 interface SidebarProps {
     sidebarOpen: boolean;
     setSidebarOpen: (sidebarOpen: boolean) => void;
 }
 const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
+    const trigger = useRef<any>(null);
+    const sidebar = useRef<any>(null);
     return (
-        <aside className={`absolute left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-black duration-300 ease-linear
-            dark:bg-boxdark lg:static lg:translate-x-0 ${sidebarOpen ? 'translate-x-0' : '-translate-x-full'
-            }`}>
+        <aside
+            ref={sidebar}
+            className={`absolute left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-black duration-300 ease-linear
+             lg:static lg:translate-x-0 ${sidebarOpen ? 'translate-x-0' : '-translate-x-full'
+                }`}>
 
             {/* <!-- SIDEBAR HEADER --> */}
             <div className="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5">
                 <Link href="/">
-                    a
+                    <Image src={coin} alt="Logo" />
                 </Link>
-
-                {/* <button
-                    ref={trigger}
-                    onClick={() => setSidebarOpen(!sidebarOpen)}
-                    aria-controls="sidebar"
-                    aria-expanded={sidebarOpen}
-                    className="block lg:hidden"
-                >
-                    <svg
-                        className="fill-current"
-                        width="20"
-                        height="18"
-                        viewBox="0 0 20 18"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            d="M19 8.175H2.98748L9.36248 1.6875C9.69998 1.35 9.69998 0.825 9.36248 0.4875C9.02498 0.15 8.49998 0.15 8.16248 0.4875L0.399976 8.3625C0.0624756 8.7 0.0624756 9.225 0.399976 9.5625L8.16248 17.4375C8.31248 17.5875 8.53748 17.7 8.76248 17.7C8.98748 17.7 9.17498 17.625 9.36248 17.475C9.69998 17.1375 9.69998 16.6125 9.36248 16.275L3.02498 9.8625H19C19.45 9.8625 19.825 9.4875 19.825 9.0375C19.825 8.55 19.45 8.175 19 8.175Z"
-                            fill=""
-                        />
-                    </svg>
-                </button> */}
             </div>
             {/* <!-- SIDEBAR HEADER --> */}
 
+            <div className=" flex flex-col overflow-y-auto duration-300 ease-linear">
+                {/* <!-- Sidebar Menu --> */}
+                <nav className="mt-5 py-4 px-4 lg:mt-9 lg:px-6 text-white">
+                    <ul className="flex flex-col gap-4 ml-4">
+                        <li>
 
+                            <Link href="/dashboard">
+                                <div className="flex items-center gap-2 ">
+                                    <Image src={dashboard} alt="Dashboard" />
+                                    <span>Dashboard</span>
+                                </div>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/profile">
+                                <div className="flex items-center gap-2 ">
+                                    <Image src={profileW} alt="Profile" />
+                                    <span>Profile</span>
+                                </div>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/settings">
+                                <div className="flex items-center gap-2 ">
+                                    <Image src={settings} alt="Settings" />
+                                    <span>Settings</span>
+                                </div>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/categories">
+                                <div className="flex items-center gap-2 ">
+                                    <Image src={categories} alt="Categories" />
+                                    <span>Categories</span>
+                                </div>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/logout">
+                                <div className="flex items-center gap-2 ">
+                                    <Image src={logout} alt="Logout" />
+                                    <span>Logout</span>
+                                </div>
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+                {/* <!-- Sidebar Menu --> */}
+            </div>
         </aside>
     )
 }
